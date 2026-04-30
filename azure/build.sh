961550e3d4a0b3c1cdcbca77f62a1b2228c5d9f4
DashLLipsey <lipseydashiell@gmail.com>
2026-04-30 17:49:27 -0400

    qpoughqpi3urbgvn2nfY

--- azure/build.sh
+++ azure/build.sh
@@ -1,5 +1,25 @@
-az login
-az acr login --name songbirdregsitry
-DOCKER_BUILDKIT=0 docker build -t songbird .
+# 1. Check what repositories exist in the registry
+az acr repository list \
+  --name songbirdregsitry \
+  --output table
+
+
+# 2. Build a clean AMD64 image locally
+DOCKER_BUILDKIT=0 docker build \
+  --platform linux/amd64 \
+  -t songbird .
+
+
+# 3. Tag the image for your Azure Container Registry
 docker tag songbird songbirdregsitry.azurecr.io/songbird:latest
+
+
+# 4. Push the AMD64 image to Azure
 docker push songbirdregsitry.azurecr.io/songbird:latest
+
+
+# 5. (Optional) Confirm the tag exists in the registry
+az acr repository show-tags \
+  --name songbirdregsitry \
+  --repository songbird \
+  --output table
